d3b7f8fa7b5531be90687ed49daa4b76333aa1e9
Luke <lukesmb@gmail.com>
2023-07-01 13:09:43 +0200

    Missed these files

---
+++ cmd/taxcalculator/transformer/sources/luno.go
@@ -0,0 +1,44 @@
+package sources
+
+import (
+	"math"
+	"strconv"
+	"time"
+
+	"github.com/ScaleneZA/CryptoTaxCalculator/cmd/taxcalculator/sharedtypes"
+)
+
+type LunoSource struct{}
+
+func (s LunoSource) TransformRow(row []string) (sharedtypes.Transaction, error) {
+	amount, err := strconv.ParseFloat(row[5], 64)
+	if err != nil {
+		return sharedtypes.Transaction{}, err
+	}
+
+	tim, err := time.Parse("2006-01-02 15:04:05", row[2])
+	if err != nil {
+		return sharedtypes.Transaction{}, err
+	}
+
+	typ := sharedtypes.TypeBuy
+	if amount < 0 {
+		amount = math.Abs(amount)
+		typ = sharedtypes.TypeSell
+	}
+
+	fiatValue, err := strconv.ParseFloat(row[12], 64)
+	if err != nil {
+		return sharedtypes.Transaction{}, err
+	}
+
+	wholePrice := fiatValue / amount
+
+	return sharedtypes.Transaction{
+		Currency:          row[4],
+		Typ:               typ,
+		Amount:            amount,
+		Timestamp:         tim.Unix(),
+		WholePriceAtPoint: wholePrice,
+	}, nil
+}
