839ce0ad0e7db125d8b1706a292c2b03b7ead1c6
Luke <lukesmb@gmail.com>
2023-07-01 20:35:39 +0200

    WIP commit

--- cmd/taxcalculator/transformer/sources/luno.go
+++ cmd/taxcalculator/transformer/sources/luno.go
@@ -1,8 +1,11 @@
 package sources
 
+// TODO(add const configuration here)
+
 import (
 	"math"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/ScaleneZA/CryptoTaxCalculator/cmd/taxcalculator/sharedtypes"
@@ -25,26 +28,44 @@ func (s LunoSource) TransformRow(row []string) (sharedtypes.Transaction, error) 
 		return sharedtypes.Transaction{}, err
 	}
 
-	typ := sharedtypes.TypeBuy
-	if amount < 0 {
-		amount = math.Abs(amount)
-		typ = sharedtypes.TypeSell
-	}
-
 	fiatValue, err := strconv.ParseFloat(row[12], 64)
 	if err != nil {
 		return sharedtypes.Transaction{}, err
 	}
 
-	wholePrice := fiatValue / amount
+	wholePrice := fiatValue / math.Abs(amount)
 
 	return sharedtypes.Transaction{
 		Currency:          mapCurrency(row[4]),
-		Typ:               typ,
-		Amount:            amount,
+		Typ:               inferType(row, amount, nil),
+		Amount:            math.Abs(amount),
 		Timestamp:         tim.Unix(),
 		WholePriceAtPoint: wholePrice,
 	}, nil
+}
+
+func inferType(row []string, amount float64, internalAddresses map[string]bool) sharedtypes.TransactionType {
+	if amount < 0 {
+		if internalAddresses[row[8]] {
+			return sharedtypes.TypeSendInternal
+		}
+		if strings.Contains(row[3], "Sold") {
+			return sharedtypes.TypeSell
+		}
+
+		if strings.Contains(row[3], "fee") {
+			return sharedtypes.TypeFee
+		}
+
+		return sharedtypes.TypeSendExternal
+	}
+
+	if strings.Contains(row[3], "Bought") {
+		return sharedtypes.TypeBuy
+	}
+
+	// Currently no way to infer external receives, assume all internal for now.
+	return sharedtypes.TypeReceiveInternal
 }
 
 func mapCurrency(s string) string {
