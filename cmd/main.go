2f98eed16c162a33ecf8d602e7e9353c947dda03
Luke <lukesmb@gmail.com>
2023-07-02 12:09:21 +0200

    WIP

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
-	"github.com/ScaleneZA/CryptoTaxCalculator/cmd/calculator"
-	"github.com/ScaleneZA/CryptoTaxCalculator/cmd/filetransformer"
 	"github.com/ScaleneZA/CryptoTaxCalculator/cmd/webhandlers"
 )
 
@@ -19,18 +16,4 @@ func main() {
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
-}
-
-func main_old() {
-	pwd, _ := os.Getwd()
-
-	ts, err := filetransformer.Transform(pwd+"/cmd/filetransformer/testData/LUNO_XBT.csv", filetransformer.TransformTypeLuno)
-	if err != nil {
-		panic(err)
-	}
-	tax, err := calculator.Calculate(ts)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(tax)
 }
--- cmd/webhandlers/uploadtransform.go
+++ cmd/webhandlers/uploadtransform.go
@@ -2,8 +2,20 @@ package webhandlers
 
 import (
 	"net/http"
+	"os"
+
+	"github.com/ScaleneZA/CryptoTaxCalculator/cmd/filetransformer"
 )
 
 func UploadTransform(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func transform(filename string) {
+	pwd, _ := os.Getwd()
+
+	_, err := filetransformer.Transform(pwd+"/cmd/filetransformer/testData/LUNO_XBT.csv", filetransformer.TransformTypeLuno)
+	if err != nil {
+		panic(err)
+	}
 }
